fe45aeea884fc172de7e8679aa2b7521a867987c
Hamza <hamzaalgharib91@gmail.com>
2026-09-23 15:32:17 +0300

    Add task storage and auto-generated ID's

--- src/main.py
+++ src/main.py
@@ -3,10 +3,22 @@ from src.models.task import Task
 
 app = FastAPI(title="Task Manager API")
 
+tasks = []
+next_task_id = 1
+
 @app.get("/")
 def root():
     return {"message":"Task Manager API is running"}
 
 @app.post("/tasks")
 def create_task(task: Task):
+    global next_task_id
+    task.id = next_task_id
+    next_task_id += 1
+    
+    tasks.append(task)
     return task
+
+@app.get("/tasks")
+def get_tasks():
+    return tasks
--- src/models/task.py
+++ src/models/task.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 
 class Task(BaseModel):
+    id: int = 0
     title: str
     description: str
     completed: bool = False
