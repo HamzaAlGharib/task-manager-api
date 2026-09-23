6c2cbb9bc8994510ffc7e46e8101a933b5bf420e
Hamza <hamzaalgharib91@gmail.com>
2026-09-23 16:07:47 +0300

    Add get-task-by-id endpoint

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from src.models.task import Task
 
 app = FastAPI(title="Task Manager API")
@@ -15,10 +15,18 @@ def create_task(task: Task):
     global next_task_id
     task.id = next_task_id
     next_task_id += 1
-    
+
     tasks.append(task)
     return task
 
 @app.get("/tasks")
 def get_tasks():
     return tasks
+
+@app.get("/tasks/{task_id}")
+def get_task(task_id: int):
+    for task in tasks:
+        if task.id == task_id:
+            return task
+        
+    raise HTTPException(status_code=404, detail="Task not found")
