9581cc1abbb203e19e67df86d19692dd9d2a6ad7
Hamza <hamzaalgharib91@gmail.com>
2026-09-23 14:57:46 +0300

    Add task model and create-task endpoint

--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from src.models.task import Task
 
 app = FastAPI(title="Task Manager API")
 
 @app.get("/")
 def root():
     return {"message":"Task Manager API is running"}
+
+@app.post("/tasks")
+def create_task(task: Task):
+    return task
